e6042ca634fca7163c175e89a9b5a61898ee2b00
tetris.dev.web <tetris.dev.web@gmail.com>
2022-02-12 22:57:09 +0800

    version 1.1.9

--- dev/assets/bold-checkout-options.js
+++ dev/assets/bold-checkout-options.js
@@ -1,2 +1,2 @@
-window.BOLD = window.BOLD || {};
+window.BOLD = window.BOLD || {};
 window.BOLD.checkout_features_defaults=[{"name":"Product Upsell","require":true,"id":"bd418c3c-a207-11e7-9971-42010af00be5"},{"name":"checkout-always-on","require":true}]
--- dev/assets/jquery.imageScroll.js
+++ dev/assets/jquery.imageScroll.js
@@ -1,14 +1,14 @@
-
-
-
-jQuery(document).ready(function(){
-	
-	
-	//.parallax(xPosition, speedFactor, outerHeight) options:
-	//xPosition - Horizontal position of the element
-	//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
-	//outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
-	jQuery('.paralax-images').parallax("50%", 0.15);
-
-
+
+
+
+jQuery(document).ready(function(){
+	
+	
+	//.parallax(xPosition, speedFactor, outerHeight) options:
+	//xPosition - Horizontal position of the element
+	//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
+	//outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
+	jQuery('.paralax-images').parallax("50%", 0.15);
+
+
 })
